57dae48e1e20265c310067c2a9203eb1f1551a9a
Aryan Sunil <psyas23@nottingham.ac.uk>
2024-11-24 14:57:39 +0000

    published chat bot

---
+++ data/songs/The Loch Ness Monster.md
@@ -0,0 +1,49 @@
+Matilda Mann - The Loch Ness Monster
+=============
+Matilda Mann - 2020
+-----------
+###Verse 1
+I'm just swimming in lakes, in my mind
+Don't make me get out and comply
+And my coy little smile will defend me a while
+So I'll stay with the lakes in my mind
+Don't you dare think that here you can hide
+We all started to stray, nothing lasts anyway
+So I'll stay with the lakes in my mind
+
+###Chorus
+And now you've faded with no traces
+So, who could ever tell?
+What you've taken's been misplaced
+But your name doesn't ring any bells
+Since the day that they took you to hell
+
+I'm just swimming in lakes, in my mind
+Too afraid to get out and deny
+And my coy little smile with defend me a while
+So I'll stay with the lakes in my mind
+
+###Chorus
+And now you've faded with no traces
+So, who could ever tell?
+What you've taken's been misplaced
+But your name doesn't ring any bells
+Since the day that they took you to hell
+See Matilda Mann Live
+Get tickets as low as $72
+You might also like
+The Fucking Best
+Matilda Mann
+The Day That I Met You
+Matilda Mann
+Die With A Smile
+Lady Gaga & Bruno Mars
+
+###Bridge
+What's it like there in hell?
+Glad they left you in hell
+Stay right down there in hell
+
+###Outro
+So I'll stay with the lakes in my mind
+Don't you dare think that here you can hide
---
+++ musicbot.py
@@ -0,0 +1,60 @@
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
+import openai 
+from dotenv import load_dotenv
+import os
+import shutil
+
+
+load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+if openai.api_key is None:
+    raise ValueError("OpenAI API key not found. Please set it in the .env file.")
+DATA_PATH = "data/songs"
+CHROMA_PATH = "chroma"
+
+def main():
+    generate_data_store()
+
+def generate_data_store():
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+
+
+
+def load_documents():
+    loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    documents = loader.load()
+    return documents
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=500,
+        length_function=len,
+        add_start_index=True,
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+    documents = chunks[10]
+    print(documents)
+    return chunks
+
+def save_to_chroma(chunks: list[Document]):
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+    db = Chroma.from_documents(
+        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+    )
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+if __name__ == "__main__":
+    main()
